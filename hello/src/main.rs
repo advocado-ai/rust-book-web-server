79c7591acbea46f6265a2cc437057872ccbb9599
advocado-ai <advocado.ai@gmail.com>
2026-08-31 10:59:29 +0800

    Refactor request handling and response generation in the connection handler

--- hello/src/main.rs
+++ hello/src/main.rs
@@ -15,51 +15,59 @@ fn main() {
 
 }
 
+enum Request{
+    GetIndex,
+    Unknown(String),
+
+}
+
+impl Request{
+    fn request_path(&self) -> &'static str{
+        match self{
+            Request::GetIndex => "GET / HTTP/1.1",
+        }
+    }
+}
+
+enum Response{
+    Ok,
+    NotFound,
+}
+
+impl Response{
+    fn status_line(&self) -> &'static str{
+        match self{
+            Response::Ok => "HTTP/1.1 200 OK",
+            Response::NotFound => "HTTP/1.1 404 NOT FOUND",
+        }
+    }
+    fn filename(&self) -> &'static str{
+        match self{
+            Response::Ok=>"hello.html",
+            Response::NotFound => "404.html",
+        }
+    }
+}
+
 fn handle_connection(mut stream: TcpStream){
     let buf_reader = BufReader::new(&stream);
+    let request_line = buf_reader.lines().next().unwrap().unwrap();
 
-    //let request_line = buf_reader.lines().next().expect("failed to read request line").expect("failed to read request line");
-
-    let http_request: Vec<_> = buf_reader
-        .lines()
-        .map(|result| result.expect("could not read request from client"))
-        .take_while(|line| !line.is_empty())
-        .collect();
-
-    println!("{:#?}", http_request);
-
-
-    if http_request[0] == "GET / HTTP/1.1"{
-        let status_line = "HTTP/1.1 200 OK";
-        let contents = fs::read_to_string("hello.html").expect("failed to read html file for response");
-        let length = contents.len();
-
-        let response = format!(
-            "{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}"
-        );
-        stream.write_all(response.as_bytes()).expect("failed to write response to TCP stream");
+    let (status_line, filename) = if request_line == Request::GetIndex.request_path() {
+        (Response::Ok.status_line(), Response::Ok.filename())
     }else{
-        let status_line = "HTTP/1.1 404 NOT FOUND";
-        let contents = fs::read_to_string("404.html").expect("failed to read 404 html");
-        let length = contents.len();
+        (Response::NotFound.status_line(), Response::NotFound.filename())
+    };
 
-        let response = format!(
-            "{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}"
-        );
-
-        stream.write_all(response.as_bytes()).expect("failed to write 404 response to tcp stream");
-    }
-
-    /*
-    let status_line = "HTTP/1.1 200 OK";
-    let contents = fs::read_to_string("hello.html").expect("failed to read html file");
+    let contents = fs::read_to_string(filename).expect("failed to read html");
 
     let length = contents.len();
 
-    let response = format!(
-        "{status_line}\r\nContent-Length: {length}\r\nContent-Type: text/html\r\n\r\n{contents}"
-    );
+    let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
 
-    stream.write_all(response.as_bytes()).expect("failed to return response");
-    */
+    stream.write_all(response.as_bytes()).expect("failed to send response");
+
+
+
+   
 }
