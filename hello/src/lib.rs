68f3c5456bcd1e947326a0d436e5d9d4e232ced9
advocado-ai <advocado.ai@gmail.com>
2026-09-01 20:20:09 +0800

    Fix enum variant naming and refactor ThreadPool initialization

--- hello/src/lib.rs
+++ hello/src/lib.rs
@@ -16,53 +16,81 @@ use std::thread;
 
 #[derive(Debug)]
 pub enum PoolCreationError{
-    NumThreadIsZero,
+    NumThreadsIsZero,
 
 }
 
+pub enum WorkerError{
+    IdNumberInvalid,
+
+}
   
 pub struct ThreadPool{
-    threads: Vec<thread::JoinHandle<()>>,
+    //threads: Vec<thread::JoinHandle<()>>,
+    workers: Vec<Worker>,
 }
 
 impl ThreadPool{
     ///Create a new ThreadPool
-        ///
-        /// Size is the number of threads in the pool
-        /// 
-        /// # Panics
-        /// 
-        /// The 'new' function panics if size is zero
+    ///
+    /// Size is the number of threads in the pool
+    /// 
+    /// # Panics
+    /// 
+    /// The 'new' function panics if size is zero
         
     pub fn new(size: usize) -> ThreadPool{
         assert!(size > 0);
 
-        let mut threads = Vec::with_capacity(size);
+        let mut workers = Vec::with_capacity(size);
 
-        for _ in 0..size{
+        for x in 0..size{
             //create some threads and store them in the vector
+            let worker = Worker::new(x as u32);
+
+            workers.push(worker);
         }
 
         ThreadPool{
-            threads
+            workers: workers,
         }
     }
 
+    /* 
     pub fn build(size: usize) -> Result<ThreadPool, PoolCreationError>{
         match size{
-            0 => Err(PoolCreationError::NumThreadIsZero),
+            0 => Err(PoolCreationError::NumThreadsIsZero),
             _ => {
                 let mut threads = Vec::with_capacity(size);
+
+                
                 Ok(ThreadPool{threads})
             },
 
         }
     }
-
+    */
     pub fn execute<F>(&self, f:F)
     where 
         F: FnOnce() + Send + 'static,
         {
 
         }
+}
+
+struct Worker{
+    id: u32,
+    handle: thread::JoinHandle<()>,
+
+}
+///Define a Worker::new function that takes an id number and returns a Worker instance that holds the id and a thread spawned with an empty closure.
+impl Worker{
+
+    fn new(id: u32) -> Worker{
+        let handler = thread::spawn(||{//thread code
+        }
+        );
+
+        Worker{id: id, handle: handler}
+    } 
 }
